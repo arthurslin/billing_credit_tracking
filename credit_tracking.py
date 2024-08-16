be2de8518a3c2e8a58a0cf604699b4c8704a9412
Arthur Lin <arthurslin.work@gmail.com>
2024-08-16 11:36:19 -0700

    created new columns & cleaned items

--- credit_tracking.py
+++ credit_tracking.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 import glob
 import os
 
@@ -11,13 +12,32 @@ def load_data(directories):
         for path in paths:
             xl = pd.ExcelFile(path)
             df = pd.read_excel(path, sheet_name=xl.sheet_names[0])
+            if df.columns[0] == "BILLING REPORT DETAIL":
+                df.columns = df.iloc[0]
+                df = df.iloc[1:].reset_index(drop=True)
         to_return.append(df)
     return to_return
 
 def get_credits(data):
-    pass
+    billing_report = data[0]
+    credits = billing_report.drop(
+        billing_report[billing_report['Part Number'] != '778752'].index, inplace=False)
+    
+    po_matches = credits["PO Number"].tolist() 
+    training_orders = billing_report[billing_report["PO Number"].isin(po_matches)] #Grab all matched items
+    training_orders["Credits"] = training_orders["Actual Ship Date"] + pd.DateOffset(years=1)
+    items_to_extract = ["Order Number","Region","BU","Product Family","Customer Name","PO Number","Part Number","Part Description","Sales Person","Order Quantity","Line Total (Local) ","Actual Ship Date","Credits"]
+    clean_items = training_orders[items_to_extract]
+    clean_items["Actual Ship Date"] = pd.to_datetime(clean_items["Actual Ship Date"]).dt.date
+    clean_items["Credits"] = pd.to_datetime(clean_items["Credits"]).dt.date
+    clean_items["Today"] = datetime.datetime.today().date()
+    clean_items["Expired?"] = clean_items["Today"] > clean_items["Credits"]
+
+    clean_items.to_excel("order.xlsx",index=False)
 
 
-dirs = []
+dirs = ["billing_report"]
 
-credit_data = load_data()
+credit_data = load_data(dirs)
+
+get_credits(credit_data)
